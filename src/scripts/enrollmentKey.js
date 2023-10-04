b4955baf9006af81f1eaee88531a092201bc402c
Noah Forchner <noah.forchner@hamburger-software.de>
2023-10-04 09:57:32 +0200

    viel kram

--- src/scripts/enrollmentKey.js
+++ src/scripts/enrollmentKey.js
@@ -1,11 +1,39 @@
-function autoFillTextField() {
-  const textField = document.getElementsByName('enrolpassword');
-  const predefinedText = 'get (user_id course_id)';
-  console.log(textField);
+// Funktion zum Speichern des vordefinierten Texts im Web Storage und Aktualisieren des Anzeige-Elements
+function savePredefinedText() {
+  const inputField = document.getElementById('enrollmentkeyinput'); // ID des Eingabefelds in index.html
+  console.log(predefinedText);
 
-  textField.forEach((textField) => {
-    textField.value = predefinedText;
-  });
+  if (inputField) {
+    const predefinedText = inputField.value;
+    localStorage.setItem('predefinedText', predefinedText);
+
+    // Aktualisieren des Anzeige-Elements
+    const displayElement = document.getElementById('displayPredefinedText');
+    if (displayElement) {
+      displayElement.innerText = `Gespeicherter Text: ${predefinedText}`;
+    }
+  }
 }
 
+// Funktion zum automatischen Ausfüllen des Textfelds
+function autoFillTextField() {
+  const textField = document.getElementsByName('enrolpasswords'); // Ersetzen Sie durch die ID des Textfelds
+  const predefinedText = 'TEST'; /* localStorage.getItem('predefinedText') */
+
+  if (textField && predefinedText) {
+    textField.forEach((textField) => {
+      textField.value = predefinedText;
+    });
+  }
+}
+
+// Aufrufen der Funktionen
 window.addEventListener('load', autoFillTextField);
+
+// Event-Handler direkt zuweisen
+const saveButton = document.getElementById('saveButton'); // ID des Speichern-Buttons in index.html
+if (saveButton) {
+  saveButton.onclick = () => {
+    savePredefinedText();
+  };
+}
